9c612e24cddca68cab4b8da2687a4954a8e8fe8d
Seyoung0914 <seyoung8529@gmail.com>
2025-08-19 15:26:54 +0900

    feat: construct project folder

--- src/app/application/user_service.py
+++
@@ -1,9 +0,0 @@
-# 유스케이스 (도메인 규칙 사용)
-from src.app.domain.user import User
-
-class UserService:
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
-
-    def get_user(self, user_id: int) -> User:
-        return self.user_repository.get_by_id(user_id)
